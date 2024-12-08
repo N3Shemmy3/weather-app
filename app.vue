48199710e341310c06c006f9356fd309b22cee4f
Shemmy <shemmynyirenda3@gmail.com>
2024-12-08 09:13:16 +0200

    Now card is complete

--- app.vue
+++ app.vue
@@ -2,7 +2,29 @@
   <App>
     <Toolbar />
 
-    <div></div>
-    <div v-for="n in 100" class="mt-4 h-16 w-full bg-colorPrimaryLight"></div>
+    <div class="w-full md:flex md:space-x-8">
+      <!--Destop left-->
+      <section
+        id="todays-specifics"
+        class="sticky top-0 md:w-[320px] space-y-4"
+      >
+        <!--Now Card-->
+        <NowCard />
+      </section>
+      <!--Destop right-->
+      <section id="todays-" class="w-full space-y-4">
+        <!--Now Card-->
+        <NowCard v-for="n in 10" />
+      </section>
+    </div>
   </App>
 </template>
+<style>
+hr {
+  @apply border-[1.2px] rounded-full dark:border-zinc-800 border-gray-200;
+}
+body {
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+    Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+}
+</style>
---
+++ components/Card/NowCard.vue
@@ -0,0 +1,27 @@
+<script setup></script>
+<template>
+  <div
+    class="aspect-square max-w-fit flex flex-col justify-center p-4 space-y-2 rounded-md border-[1.5px] dark:border-zinc-800 border-gray-200"
+  >
+    <h4 class="text-lg md:text-xl">Now</h4>
+    <div class="w-full flex justify-between items-center">
+      <!--Current city temp-->
+      <h2 id="today-current-temp" class="text-8xl">
+        25<sup>&deg;</sup><span class="text-5xl">C</span>
+      </h2>
+      <!--Current city temp icon-->
+      <Icon name="meteocons:clear-day-fill" size="120" />
+    </div>
+
+    <h4 id="today-current-condition" class="text-lg">Cloudy</h4>
+    <hr />
+    <h4 id="today-current-city" class="flex space-x-1 items-center text-lg">
+      <Icon name="ic:outline-calendar-month" size="24" />
+      <span> Sunday 8, Dec</span>
+    </h4>
+    <h4 id="today-current-city" class="flex space-x-1 items-center text-lg">
+      <Icon name="ic:outline-location-on" size="24" />
+      <span> Lusaka, Zambia </span>
+    </h4>
+  </div>
+</template>
